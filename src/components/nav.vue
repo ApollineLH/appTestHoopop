61dd43bcfeb2db97654d56391fb7947469fa9f1f
apolline <apolline.le@gmail.com>
2022-05-02 18:21:33 +0200

    fix: nav

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,6 +1,6 @@
 <template>
     <b-navbar toggleable type="dark" variant="dark">
-        <b-navbar-brand href="/Helloworld">Micro shop</b-navbar-brand>
+        <b-navbar-brand href="/shop">Micro shop</b-navbar-brand>
         <b-navbar-nav class="ml-auto">
             <router-link to='/cart' size="sm" class="mb-2">
                 <b-icon icon="cart" aria-hidden="true"></b-icon> Cart
